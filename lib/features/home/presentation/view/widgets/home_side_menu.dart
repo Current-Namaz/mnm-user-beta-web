c9a132df37c4465d3e3185409ada211b9b6ce1bf
ahmedteli9909 <ateli7257@gmail.com>
2024-01-06 19:21:26 +0530

    last 4 commit

--- lib/features/home/presentation/view/widgets/home_side_menu.dart
+++ lib/features/home/presentation/view/widgets/home_side_menu.dart
@@ -3,7 +3,7 @@ import 'package:mnm_internal_admin/core/values/app_colors.dart';
 import 'package:mnm_internal_admin/core/values/app_images.dart';
 import 'package:mnm_internal_admin/core/values/app_strings.dart';
 import 'package:mnm_internal_admin/core/values/app_styles.dart';
-import 'package:mnm_internal_admin/core/widgets/Global_logo.dart';
+import 'package:mnm_internal_admin/core/widgets/global_logo.dart';
 import 'package:mnm_internal_admin/core/widgets/common_image.dart';
 import 'package:mnm_internal_admin/features/home/presentation/view/widgets/side_menu_widget.dart';
 
