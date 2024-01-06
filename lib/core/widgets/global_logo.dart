45d5d33677686023c69e3ca27b788252839d0cea
ahmedteli9909 <ateli7257@gmail.com>
2024-01-06 18:50:51 +0530

    test commit

---
+++ lib/core/widgets/global_logo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mnm_internal_admin/core/values/app_images.dart';
+import 'package:mnm_internal_admin/core/widgets/common_image.dart';
+
+class GlobalLogo extends StatelessWidget {
+  const GlobalLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         CommonImage.asset(AppImages.appForGroundLogo,height: 45,width: 40,),
+         const SizedBox(width: 3,),
+         CommonImage.asset(AppImages.appTextLogo,height: 50,)
+
+        ],
+      ),
+    );
+  }
+}
